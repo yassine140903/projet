1f76eeaf6f012541f7d11fadc1f07fd8fd3aa7ad
yassine140903 <154000479+yassine140903@users.noreply.github.com>
2024-11-14 13:51:03 +0100

    product

--- marketplace/src/app/app.component.ts
+++ marketplace/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from './signup/signup.component';
-
+import { ProductPageComponent } from './product/product.component';
+  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent , SignupComponent , RouterLink,RouterLinkActive],
+  imports: [RouterOutlet, LoginComponent , SignupComponent , RouterLink,RouterLinkActive, ProductPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ marketplace/src/app/product/product.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class ProductPageComponent {
+  product = {
+    name: 'Product Name',
+    price: 88.67,
+    sizes: [40, 42, 44],
+    colors: ['Black', 'White'],
+    description: 'Product description goes here...',
+  };
+  productImages = ['assets/img/Earth Tones and Musical Touches_ Elegant Clothing on Coat Racks.jpg',
+    'assets/img/5M.jpg',
+  ];
+  mainImage = this.productImages[0];
+  selectedSize = this.product.sizes[0];
+  selectedColor = this.product.colors[0];
+  suggestedProducts = [
+    { name: 'Suggested Product 1', image: 'suggested1.jpg' },
+    { name: 'Suggested Product 2', image: 'suggested2.jpg' }
+  ];
+
+  setMainImage(image: string) {
+    this.mainImage = image;
+  }
+
+  addToCart() {
+    console.log('Product added to cart:', this.product.name);
+  }
+
+  currentImageIndex = 0;
+  nextImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.productImages.length;
+    this.mainImage = this.productImages[this.currentImageIndex];
+  }
+  previousImage() {
+    this.currentImageIndex = (this.currentImageIndex - 1 + this.productImages.length) % this.productImages.length;
+    this.mainImage = this.productImages[this.currentImageIndex];
+  }
+}
